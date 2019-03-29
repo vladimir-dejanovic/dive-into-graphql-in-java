7b9f370dffa8e493284cdd79cd175e282df588e2
Vladimir Dejanovic <vladimir@itshark.xyz>
2019-03-29 23:37:32 +0100

    update code to use latest version of graphql-java and Java 11

--- src/main/java/xyz/itshark/conf/tutorial/diveintographqlinjava/DiveIntoGraphqlInJavaApplication.java
+++ src/main/java/xyz/itshark/conf/tutorial/diveintographqlinjava/DiveIntoGraphqlInJavaApplication.java
@@ -1,10 +1,17 @@
 package xyz.itshark.conf.tutorial.diveintographqlinjava;
 
+import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.SimpleGraphQLHttpServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import xyz.itshark.conf.tutorial.diveintographqlinjava.resolver.Mutation;
+import xyz.itshark.conf.tutorial.diveintographqlinjava.resolver.Query;
+import xyz.itshark.conf.tutorial.diveintographqlinjava.resolver.SpeakerReslover;
+import xyz.itshark.conf.tutorial.diveintographqlinjava.resolver.TalkReslover;
 import xyz.itshark.conf.tutorial.diveintographqlinjava.service.AttendeeService;
 import xyz.itshark.conf.tutorial.diveintographqlinjava.service.SpeakerService;
 import xyz.itshark.conf.tutorial.diveintographqlinjava.service.TalkService;
@@ -28,9 +35,27 @@ public class DiveIntoGraphqlInJavaApplication {
 
 	@Bean
 	public ServletRegistrationBean graphQLServlet() {
-		GraphQLEntryPoint graphQLEntryPoint = new GraphQLEntryPoint(speakerService,attendeeService,talkService);
+//		GraphQLEntryPoint graphQLEntryPoint = new GraphQLEntryPoint(speakerService,attendeeService,talkService);
 
-		return new ServletRegistrationBean(graphQLEntryPoint,"/graphql");
+		return new ServletRegistrationBean(SimpleGraphQLHttpServlet.newBuilder(buildSchema(speakerService,attendeeService,talkService)).build(),"/graphql");
 	}
+
+
+
+
+	private static GraphQLSchema buildSchema(SpeakerService speakerService, AttendeeService attendeeService, TalkService talkService) {
+		return SchemaParser
+				.newParser()
+				.file("schema.graphqls")
+//                .dictionary()
+				.resolvers( new Query(attendeeService,speakerService,talkService),
+						new TalkReslover(speakerService),
+						new SpeakerReslover(talkService),
+						new Mutation(speakerService))
+				.build()
+				.makeExecutableSchema();
+	}
+
+
 
 }
